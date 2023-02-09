b5f611364bef24cfcc4c3d3fe107222408014126
jyeoniii <jaykim9438@gmail.com>
2023-02-09 13:24:28 +0900

    feat: configure L1 & L2 blockscout

--- ops-bedrock/kube-configure-blockscout.sh
+++ ops-bedrock/kube-configure-blockscout.sh
@@ -2,19 +2,19 @@ helm repo add bitnami https://charts.bitnami.com/bitnami
 helm install redis --set architecture=standalone bitnami/redis
 helm install postgres \
   --set global.postgresql.auth.postgresPassword=password \
-  --set global.postgresql.auth.database=blockscout \
   --set-string 'primary.extraEnvVars[0].name=POSTGRESQL_MAX_CONNECTIONS,primary.extraEnvVars[0].value=500' \
+  --set primary.initdb.scripts."init\.sql"='create database blockscout_l1;create database blockscout_l2;' \
   bitnami/postgresql
 
-kubectl wait --namespace default \
-  --for=condition=ready pod \
-  --selector=app.kubernetes.io/name=postgresql \
-  --timeout=90s
-
-kubectl wait --namespace default \
-  --for=condition=ready pod \
-  --selector= app.kubernetes.io/name=redis \
-  --timeout=90s
+kubectl wait --for=condition=ready pod --selector=app.kubernetes.io/name=postgresql --timeout=90s
+kubectl wait --for=condition=ready pod --selector=app.kubernetes.io/name=redis --timeout=90s
 
 kubectl create cm blockscout-cm --from-env-file=blockscout.env
-kubectl create -f blockscout.yaml
+kubectl create cm smart-contract-verifier-cm --from-env-file=smart-contract-verifier.env
+
+# TODO: Change to use helm chart
+kubectl create -f blockscout-l1.yaml
+kubectl create -f blockscout-l2.yaml
+
+kubectl wait --for=condition=ready pod --selector=app=blockscout_l1 --timeout=90s
+kubectl wait --for=condition=ready pod --selector=app=blockscout_l2 --timeout=90s
--- ops-bedrock/kube-configure.sh
+++ ops-bedrock/kube-configure.sh
@@ -1,4 +1,4 @@
-# Create Kind Cluster
+# Create Kind Cluster (NOTE: You can install kind by running `brew install kind`)
 kind create cluster --config=config-with-mounts.yaml
 
 # Load Docker image
@@ -8,6 +8,25 @@ kind load docker-image ops-bedrock_op-node
 kind load docker-image ops-bedrock_op-proposer
 kind load docker-image ops-bedrock_op-batcher
 kind load docker-image ops-bedrock_stateviz
+kind load docker-image blockscout/blockscout
+
+kubectl create -f l1.yaml
+kubectl create -f l2.yaml
+
+kubectl wait --for=condition=ready pod --selector=app=l1 --timeout=90s
+kubectl wait --for=condition=ready pod --selector=app=l2 --timeout=90s
+
+# Configure Blockscout
+sh kube-configure-blockscout.sh
+
+kubectl create -f op-log-pv.yaml
+kubectl create -f op-node.yaml
+kubectl wait --for=condition=ready pod --selector=app=op-node --timeout=90s
+
+kubectl create -f op-proposer.yaml
+kubectl create -f op-batcher.yaml
+kubectl create -f stateviz.yaml
+kubectl create -f ingress.yaml
 
 # Deploy Ingress NGINX
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
@@ -18,16 +37,9 @@ kubectl wait --namespace ingress-nginx \
   --selector=app.kubernetes.io/component=controller \
   --timeout=90s
 
-kubectl create -f l1.yaml
-kubectl create -f l2.yaml
-kubectl create -f op-log-pv.yaml
-kubectl create -f op-node.yaml
-kubectl create -f op-proposer.yaml
-kubectl create -f op-batcher.yaml
-kubectl create -f stateviz.yaml
-kubectl create -f ingress.yaml
-
 echo "Successfully configured optimism network!"
-echo " > L1 RPC: http://localhost/l1"
-echo " > L2 RPC: http://localhost/l2"
-echo " > OP Node RPC: http://localhost/op-node"
+echo " > L1 RPC: http://l1-rpc.iskra.world"
+echo " > L2 RPC: http://l2-rpc.iskra.world"
+echo " > OP Node RPC: http://op-node-rpc.iskra.world"
+echo " > L1 Explorer: http://l1-explorer.iskra.world"
+echo " > L2 Explorer: http://l2-explorer.iskra.world"
